4a8bcadbbe94985e694a3735c7a2ac002ddaf295
leeyesol <42531341+leeyesol@users.noreply.github.com>
2022-10-14 20:54:46 +0900

    basic task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-function App() {
-  return <div className='App' />;
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    // 로딩된 애니 데이터 저장
+    animations: [],
+  };
+
+  componentDidMount(){
+    // 애니 데이터 로딩
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+  }
+
+  render(){
+    const {isLoading} = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const rootNode = document.getElementById('root');
 
-ReactDOM.render(
-   <App/>,
-  document.getElementById('root')
+ReactDOM.createRoot(rootNode).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
